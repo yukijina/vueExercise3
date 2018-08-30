f6a268cd09587ea00c58f54584d55c1ce0653da4
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-08-29 21:40:36 -0700

    loop

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ new Vue ({
     ]
   }
 })
+
+new Vue ({
+  el: "#exercise",
+  data: {
+    display: true,
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkoens',
+      books: '3'
+    },
+    testData: {
+      name: 'TESTOBJECT',
+      id: 10,
+      data: [1.67,1.33,0.98, 2.21]
+    }
+  }
+});
